b63e4bda82fc301e02747e4a4f0a790954f5f6aa
Oleg Podsechin <oleg.podsechin@ionsquare.com>
2011-09-18 09:56:14 +0300

    example rename

--- examples/friendfeed.js
+++
@@ -1,6 +0,0 @@
-var generate = require('../lib/restwrapper').generate;
-var api = {
-	"feed/{feed}": ["GET"]
-};
-var ff = generate("http://friendfeed-api.com/v2/", api);
-console.log(ff.getFeed("oleg"));
